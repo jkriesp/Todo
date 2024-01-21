bfaebece87f16d5f30aa6404575a93524f58badf
Jens Kristian Espevik <jenskristian+githubpro@gmail.com>
2024-01-21 10:48:01 +0100

    Add validation for title and description

--- src/main/java/net/jenske/todo/model/Todo.java
+++ src/main/java/net/jenske/todo/model/Todo.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 
 import java.util.Date;
 
@@ -12,7 +15,11 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min = 1, max = 100) // Adjust size constraints as needed
+    @Pattern(regexp = "^[a-zA-Z0-9\\s.,]+$", message = "Title must be alphanumeric and can include spaces, dots, and commas")
     private String title;
+    @Pattern(regexp = "^[a-zA-Z0-9\\s.,]+$", message = "Description must be alphanumeric and can include spaces, dots, and commas")
+    @Size(max = 500) // Adjust size constraints as needed
     private String description;
     @JsonFormat(pattern="yyyy-MM-dd")
     private Date dueDate;
